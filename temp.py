6951a098764ccb0c60da2ece44439d19fa184d9a
Abhiram Nair <abhirammnair@gmail.com>
2020-10-30 14:14:24 +0530

    Added some data visualization

--- temp.py
+++ temp.py
@@ -1,7 +1,5 @@
 import pandas as pd
 
-#download it from kaggle and unzip each file and put in the folder named below
-
 df = pd.read_csv("jigsaw-toxic-comment-classification-challenge/train.csv",encoding='latin-1')
 
 del df["id"]
